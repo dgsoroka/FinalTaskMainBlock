a5c3dced7490b81aeb04cc70315bb8cf2da884b4
Soroka Dmitriy <dima.soroka.2014@mail.ru>
2023-04-08 17:34:50 +1000

    Created sort method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+
+
+
+
+
+
+String[] OnlySmallerOrEqualThreeArray(String[] array){
+  int newArraySize = 0;
+
+  for(int i = 0; i < array.Length; i++){
+    if(array[i].Length <= 3){
+      newArraySize++;
+    }
+  }
+
+  String[] newArray = new String[newArraySize];
+
+  for(int i = 0; i < array.Length; i++){
+    if(array[i].Length <= 3){
+      newArray.Append(array[i]);
+    }
+  }
+
+  return newArray;
+}
