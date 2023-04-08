f03ed0e7e8537fa22633d939c0b7d2aabb328125
Soroka Dmitriy <dima.soroka.2014@mail.ru>
2023-04-08 17:51:20 +1000

    Added FillArray method

--- Program.cs
+++ Program.cs
@@ -6,24 +6,42 @@
 
 
 
+Console.Write("Введите длину массива строк: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+String[] stringArray = new String[arrSize];
+FillArray(stringArray);
 
 
+void FillArray(String[] array)
+{
+  for (int j = 0; j < stringArray.Length; j++)
+  {
+    String el = Console.ReadLine();
+    stringArray[j] = el;
+  }
+}
 
 
-String[] OnlySmallerOrEqualThreeArray(String[] array){
+String[] OnlySmallerOrEqualThreeArray(String[] array)
+{
   int newArraySize = 0;
+  int count = 0;
 
-  for(int i = 0; i < array.Length; i++){
-    if(array[i].Length <= 3){
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
       newArraySize++;
     }
   }
 
   String[] newArray = new String[newArraySize];
 
-  for(int i = 0; i < array.Length; i++){
-    if(array[i].Length <= 3){
-      newArray.Append(array[i]);
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      newArray[count++] = array[i];
     }
   }
 
